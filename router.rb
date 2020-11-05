c9e1570773e90d9bb017d0d1beea8d3541420cb3
Yann Klein <yann.klein@me.com>
2020-11-05 13:19:06 +0900

    livecode base

---
+++ router.rb
@@ -0,0 +1,45 @@
+class Router
+  def initialize(meals_controller, customers_controller)
+    @meals_controller = meals_controller
+    @customers_controller = customers_controller
+    @running = true
+  end
+
+  def run
+    while @running
+      print_menu
+      choice = gets.chomp.to_i
+      print `clear`
+      route_action(choice)
+    end
+  end
+
+  private
+
+  def print_menu
+    puts "--------------------"
+    puts "------- MENU -------"
+    puts "--------------------"
+    puts "1. Add new meal"
+    puts "2. List all meals"
+    puts "3. Add new customer"
+    puts "4. List all customers"
+    puts "8. Exit"
+    print "> "
+  end
+
+  def route_action(choice)
+    case choice
+    when 1 then @meals_controller.add
+    when 2 then @meals_controller.list
+    when 3 then @customers_controller.add
+    when 4 then @customers_controller.list
+    when 8 then stop!
+    else puts "Try again..."
+    end
+  end
+
+  def stop!
+    @running = false
+  end
+end
